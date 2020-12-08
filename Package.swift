43aa9c04d60b0be5d3140f0e8a23e6670371f51b
Srinivas Prabhu <srinivas@codezyng.com>
2020-12-08 19:07:26 +0530

    Updated source path in spm file

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftState",
-            dependencies: []),
+            dependencies: [],
+            path:"Sources"),
         .testTarget(
             name: "SwiftStateTests",
-            dependencies: ["SwiftState"]),
+            dependencies: ["SwiftState"],
+            path:"Sources"),
     ]
 )

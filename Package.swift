fbc36eab93a59a368e5f24a3b4902f1b941b1d39
Srinivas Prabhu <srinivas@codezyng.com>
2020-12-08 18:37:11 +0530

    spm support

--- Package.swift
+++ Package.swift
@@ -10,4 +10,12 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftState"
+    name: "SwiftState",
+    platforms: [.iOS(.v11),.watchOS(*)],
+    products: [
+       .library(name: "SwiftState", targets: ["SwiftState"])
+    ],
+    targets: [
+       .target(name: "SwiftState", path: "Sources")
+    ]
 )

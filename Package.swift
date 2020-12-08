9f3f97b9e065d9cc9f6d29d40ff6e6a7ae82e157
Srinivas Prabhu <srinivas@codezyng.com>
2020-12-08 18:47:36 +0530

    swift package manager support

--- Package.swift
+++ Package.swift
@@ -1,20 +1,28 @@
-//
-//  Package.swift
-//  SwiftState
-//
-//  Created by Yasuhiro Inami on 2015-12-05.
-//  Copyright © 2015 Yasuhiro Inami. All rights reserved.
-//
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "SwiftState",
-    platforms: [.iOS(.v11),.watchOS(*)],
     products: [
-       .library(name: "SwiftState", targets: ["SwiftState"])
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "SwiftState",
+            targets: ["SwiftState"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-       .target(name: "SwiftState", path: "Sources")
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "SwiftState",
+            dependencies: []),
+        .testTarget(
+            name: "SwiftStateTests",
+            dependencies: ["SwiftState"]),
     ]
 )
